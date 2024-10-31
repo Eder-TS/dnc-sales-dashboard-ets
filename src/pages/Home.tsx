1d5da6f43e72e0b86782d78fc04015b8d14d8f52
Eder-TS <steder20@yahoo.com.br>
2024-10-31 20:33:46 -0300

    Feat: Avatar list component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,33 @@
-import { Header, CardComponent } from '@/components'
+import { Header, CardComponent, AvatarList } from '@/components'
+import { currencyConverter } from '@/utils'
 import { Container } from '@mui/material'
 
 function Home() {
+  const mockListData = [
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencyConverter(6546.16),
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 2',
+      subtitle: currencyConverter(3246.14),
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 3',
+      subtitle: currencyConverter(2169.87),
+    },
+  ]
   return (
     <>
       <Header />
       <Container maxWidth="lg">
-        <CardComponent className='success'>CARD</CardComponent>
+        <CardComponent>CARD</CardComponent>
+        <CardComponent>
+          <AvatarList listData={mockListData} />
+        </CardComponent>
       </Container>
     </>
   )
--- src/utils/pxToRem.ts
+++ src/utils/pxToRem.ts
@@ -3,6 +3,6 @@
  * @param pixels - Pixels value to be converted.
  * @returns - The converted rem value.
  */
-export function pxToRem(pixels: number) {
+export function pxToRem(pixels: number): string {
   return `${pixels / 16}rem`
 }
