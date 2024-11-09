8cd78955061738cc23852316e394dd94bda8ae2d
Eder-TS <steder20@yahoo.com.br>
2024-11-09 13:20:13 -0300

    fix/.env

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   optimizeDeps: {
     include: ['@emotion/styled'],
   },
