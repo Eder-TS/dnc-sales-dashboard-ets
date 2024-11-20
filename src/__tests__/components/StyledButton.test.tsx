23989c160915f50558cd109013de65d40dc0b6c7
Eder-TS <steder20@yahoo.com.br>
2024-11-20 18:42:41 -0300

    Feat: Adding unit tests

---
+++ src/__tests__/components/StyledButton.test.tsx
@@ -0,0 +1,37 @@
+import { themesList } from '@/resources/themesList'
+import { StyledButton } from '@/components'
+import { render } from '@testing-library/react'
+import { Theme } from '@/types'
+import { ThemeProvider } from 'styled-components'
+
+describe('StyledButton', () => {
+    const renderComponent = (theme: Theme, className?: string, props = {}) => render(
+        <ThemeProvider theme={theme}>
+            <StyledButton className={className} {...props} />
+        </ThemeProvider>
+    )
+
+    themesList.forEach(({name, theme}) => {
+        describe(`${name}`, () => {
+            it ('should match snapshot with alert class', () => {
+                const { asFragment } = renderComponent(theme, 'alert')
+                expect(asFragment()).toMatchSnapshot()
+            })
+
+            it ('should match snapshot with primary class', () => {
+                const { asFragment } = renderComponent(theme, 'primary')
+                expect(asFragment()).toMatchSnapshot()
+            })
+
+            it ('should match snapshot with borderless-alert class', () => {
+                const { asFragment } = renderComponent(theme, 'borderless-alert')
+                expect(asFragment()).toMatchSnapshot()
+            })
+
+            it ('should match snapshot with disabled status', () => {
+                const { asFragment } = renderComponent(theme, 'primary', {disabled: true})
+                expect(asFragment()).toMatchSnapshot()
+            })
+        })
+    })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 import mkcert from 'vite-plugin-mkcert'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react(), mkcert(), tsconfigPaths()],
   optimizeDeps: {
     include: ['@emotion/styled'],
   },
